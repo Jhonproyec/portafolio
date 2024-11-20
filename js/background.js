4f772a8e1f8aeef03f00ba1d1ec5f7041399e5f7
jacastro <jacastro@mspas.gob.gt>
2024-11-20 08:51:55 -0600

    Quitar background de bolitas

--- js/background.js
+++ js/background.js
@@ -3,40 +3,41 @@ const numBalls = 30;
 const balls = [];
 const container = document.getElementsByClassName('background-balls')[0]; 
 
-for (let i = 0; i <= numBalls; i++) {
-   let ball = document.createElement("div");
-   ball.classList.add('ball');
-   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-   ball.style.width = `${Math.random() + 0.5}em`;
-   ball.style.height = ball.style.width;
-   ball.style.position = "absolute"; 
-   ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-   ball.style.top = `${Math.floor(Math.random() * 70)}vh`;
+// for (let i = 0; i <= numBalls; i++) {
+//    let ball = document.createElement("div");
+//    ball.classList.add('ball');
+//    ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+//    ball.style.width = `${Math.random() + 0.5}em`;
+//    ball.style.height = ball.style.width;
+//    ball.style.position = "absolute";
+//    ball.style.alignItems = "center" 
+//    ball.style.left = `${Math.floor(Math.random() * 50)}vw`;
+//    ball.style.top = `${Math.floor(Math.random() * 50)}vh`;
    
-   balls.push(ball);
-   container.appendChild(ball); 
-}
-balls.forEach((el, i, ra) => {
-   let to = {
-      x: Math.random() * (i % 2 === 0 ? -11 : 11),
-      y: Math.random() * 12
-   }
+//    balls.push(ball);
+//    container.appendChild(ball); 
+// }
+// balls.forEach((el, i, ra) => {
+//    let to = {
+//       x: Math.random() * (i % 2 === 0 ? -11 : 11),
+//       y: Math.random() * 12
+//    }
 
 
-   let anim = el.animate(
-      [
-         { transform: "translate(0,0)" },
-         { transform: `translate(${to.x}rem, ${to.y}rem)` }
-      ],
-      {
-         duration: (Math.random() + 1) * 1500,
-         iterations: Infinity,
-         fill: "both",
-         direction: "alternate",
-         easing: "ease-in-out"
-      }
-   )
-});
+//    let anim = el.animate(
+//       [
+//          { transform: "translate(0,0)" },
+//          { transform: `translate(${to.x}rem, ${to.y}rem)` }
+//       ],
+//       {
+//          duration: (Math.random() + 1) * 1500,
+//          iterations: Infinity,
+//          fill: "both",
+//          direction: "alternate",
+//          easing: "ease-in-out"
+//       }
+//    )
+// });
 
 
 
